3678cbc9c98df57d3a55e37d5adc730d785dfbeb
ash <anaa.ceban@gmail.com>
2025-03-21 21:42:08 +0100

    interactive book finished

--- src/levels/book_scene.gd
+++ src/levels/book_scene.gd
@@ -8,10 +8,29 @@ var current_page: int = 0
 @onready var prev_button = $ButtonPrev
 @onready var next_button = $ButtonNext
 
+var direction_right: bool = true
+
+var forward_animations = [
+	"first_page",
+	"second_page",
+	"third_page",
+	"fourth_page",
+	"last_page",
+	"close_book",
+	"return_book"
+	]
+var page_contents = [
+	["", ""],
+	["", "page3 content"],
+	["page4 content", "page5 content"],
+	["page6 content", "page7 content"],
+	["page7 content", "page8 content"],
+	["page9 content", ""],
+	["", ""],
+	]
+
 func _ready() -> void:
-	right_page_text.visible = false
-	left_page_text.visible = false
-	prev_button.visible = false
+	_update_buttons()
 	book_sprite.connect("animation_finished", Callable(self, "_on_animation_finished"))
 
 func _clear_pages():
@@ -19,52 +38,31 @@ func _clear_pages():
 	left_page_text.visible = false
 
 func _play_animation():
-	# TODO: switch the text
-	if current_page == 0:
-		book_sprite.play("first_page")
-		return
-	if current_page == 1:
-		_clear_pages()
-		book_sprite.play("second_page")
-		return 
-	if current_page == 2:
-		_clear_pages()
-		book_sprite.play("third_page")
-		return 
-	if current_page == 3:
-		_clear_pages()
-		book_sprite.play("fourth_page")
-		return 
-	if current_page == 4:
-		_clear_pages()
-		book_sprite.play("last_page")
-		return 
-	if current_page == 5:
-		_clear_pages()
-		book_sprite.play("close_book")
-		return 
+	_clear_pages()
+	if self.direction_right:
+		book_sprite.play(forward_animations[current_page])
+	else:
+		book_sprite.play_backwards(forward_animations[current_page])
 
 func _on_button_next_pressed() -> void:
+	if self.current_page >= 6:
+		self.current_page = 0
+	self.direction_right = true
 	_play_animation()
 	self.current_page += 1
 
-
 func _on_button_prev_pressed() -> void:
-	# TODO: play the backwards animation
-	pass # Replace with function body.
+	self.direction_right = false
+	self.current_page -= 1
+	_play_animation()
 
 func _on_animation_finished():
+	right_page_text.text = page_contents[current_page][1]
+	left_page_text.text = page_contents[current_page][0]
 	right_page_text.visible = true
-	# première page donc texte que sur la gauche
-	if current_page != 1 :
-		left_page_text.visible = true
-	if current_page in range(1, 5):
-		prev_button.visible = true
+	left_page_text.visible = true
+	prev_button.visible = true
 
-	# dernière page donc texte que sur la gauche
-	if current_page == 5:
-		right_page_text.visible = false
-	# on ferme le livre
-	if current_page == 6:
-		next_button.visible = false
-		_clear_pages()
+func _update_buttons():
+	if current_page == 0:
+		prev_button.visible = false
