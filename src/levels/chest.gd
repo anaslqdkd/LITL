483d31e3f78d6a52d6634436f3f73fdfbb1fcbe0
ash <anaa.ceban@gmail.com>
2025-03-24 18:45:30 +0100

    notifications, assets etc

--- src/levels/chest.gd
+++ src/levels/chest.gd
@@ -3,13 +3,18 @@ extends Interactable
 var current_scene = null
 var player = null
 
+@export var item: InvItem
+@export var chest_open_sound: AudioStreamPlayer
+@export var pick_item_sound: AudioStreamPlayer
 
 func _on_interacted() -> void:
 	current_scene = get_tree().current_scene
 	player = current_scene.get_node_or_null("Player")
-	var apple_item = preload("res://src/inventory/items/explosion.tres")
-	if player.has_item(apple_item):
-		print("TAAAAAAAAAAAAAAAAAA")
+	var magnifying_glass = preload("res://src/inventory/items/magnifying_glass.tres")
+	var key = preload("res://src/inventory/items/explosion.tres")
+	if player.has_item(key):
+		NotificationPanel.send_notification("Chest unlocked")
+		player.collect(magnifying_glass)
 	else:
-		print("BYYYYYYYYYYYYYYYYQEEEEEEE")
+		NotificationPanel.send_notification("The chest is locked")
 	
---
+++ src/ui/notification_panel.gd
@@ -0,0 +1,18 @@
+extends Node
+
+@onready var notification_label = $Label
+@export var notification_sound: AudioStreamPlayer
+
+func _ready() -> void:
+	self.visible = false
+
+func send_notification(message: String, duration: float = 3.0):
+	notification_sound.play()
+	self.visible = true
+	notification_label.text = message
+	
+	notification_label.visible = true
+	await get_tree().create_timer(duration).timeout 
+	self.visible = false 
+	notification_label.visible = false
+	
