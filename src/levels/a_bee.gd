ec0f1c06bf582da869f91f38cb069c74937c7001
Lowell <lowel.buzzi@telecomnancy.eu>
2025-03-26 16:53:18 +0100

    Update and fix : light manage to have higher framerate, second book put, first enigma almost finished

---
+++ src/levels/a_bee.gd
@@ -0,0 +1,15 @@
+extends Interactable
+
+var book_instance = null
+var book_scene = preload("res://src/levels/book_scene.tscn")
+var current_scene = null
+var player = null
+
+func _on_interacted() -> void:
+	current_scene = get_tree().current_scene
+	player = current_scene.get_node_or_null("Player")
+	if not book_scene:
+		return
+	self.book_instance = book_scene.instantiate()
+	self.book_instance.book = "a_bee"
+	add_child(book_instance)
