e8c66e3601678d168fcafd75e650a8c5f7b43f38
Lowell <lowel.buzzi@telecomnancy.eu>
2025-03-29 13:17:41 +0100

    Update : Third engimas added. Leveldesign partially done

---
+++ src/levels/empty_set.gd
@@ -0,0 +1,16 @@
+extends Interactable
+
+var paper_instance = null
+var paper_scene = preload("res://src/levels/paper_scene.tscn")
+
+func _on_interacted() -> void:
+	if not paper_scene:
+		return
+
+	paper_instance = paper_scene.instantiate()
+	if paper_instance.has_method("_ready") and "note" in paper_instance:
+		paper_instance.note = "empty_set" 
+	else:
+		print("Error: 'note' property not found in paper_instance")
+	
+	add_child(paper_instance)
